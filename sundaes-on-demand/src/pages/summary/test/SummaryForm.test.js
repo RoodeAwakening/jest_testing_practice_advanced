8ebe0deb9e343559e52f85039a9917716dd156d7
Derek Roode <droode@nexient.com>
2022-05-02 17:01:11 -0400

    new test

--- sundaes-on-demand/src/pages/summary/test/SummaryForm.test.js
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.js
@@ -1,5 +1,6 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import SummaryForm from '../SummaryForm';
+import userEvent from '@testing-library/user-event';
 
 describe('summary form', () => {
   it('should have initial conditions', () => {
@@ -26,10 +27,37 @@ describe('summary form', () => {
       name: /confirm order/i,
     });
 
-    fireEvent.click(checkbox);
+    userEvent.click(checkbox);
     expect(confirmButton).toBeEnabled();
 
-    fireEvent.click(checkbox);
+    userEvent.click(checkbox);
     expect(confirmButton).toBeDisabled();
+  });
+
+  it('should have a popover that responds to hover', () => {
+    render(<SummaryForm />);
+
+    // popover starts hidden
+    const nullPopover = screen.queryByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(nullPopover).not.toBeInTheDocument();
+
+    //appears upon mouseover of checkbox label
+    const termsAndConditions = screen.getByText(/terms and conditions/i);
+    userEvent.hover(termsAndConditions);
+
+    const popover = screen.getByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(popover).toBeInTheDocument();
+
+    // popover disappears when mouse out
+    userEvent.unHover(termsAndConditions);
+    const nullPopoverAgain = screen.queryByText(
+      /no ice cream will actually be delivered/i
+    );
+
+    expect(nullPopoverAgain).not.toBeInTheDocument();
   });
 });
